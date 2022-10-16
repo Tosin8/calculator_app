00b56f2b0669f6dc327f39deb806068cd1cc2368
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-16 17:42:15 +0100

    Update homepage.dart

--- lib/widget/homepage.dart
+++ lib/widget/homepage.dart
@@ -13,6 +13,8 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12.0),
+      height: 80.0,
       child: Scaffold(
         appBar: AppBar(
           title: const Text(
@@ -34,7 +36,30 @@ class _HomePageState extends State<HomePage> {
             child: Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: Column(
-                  children: const [],
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        radioButton('Man', Colors.blue, 0),
+                        radioButton('Woman', Colors.pink, 1),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    const Text('Your Height in Cm: ',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        )),
+                    const TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          hintText: 'Your height in Cm', filled: true),
+                    )
+
+                    // creating the input form.
+                  ],
                 ))),
       ),
     );
@@ -53,7 +78,30 @@ class _HomePageState extends State<HomePage> {
   Widget radioButton(String value, Color color, int index) {
     return Expanded(
         child: Container(
-      child: FlatButton(),
+      child: ElevatedButton(
+        //  makeing the color of the button change in function of it's selected or not.
+
+        color: currentindex == index
+            ? color
+            : Colors.grey[
+                200], // if the current index is equal to the button index then put the color , the main color that will define otherwise make it grey.
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ), // adding the round button.
+        onPressed: () {
+          // clicking the button , the current index will have the value of the button's index.
+          changeIndex(index);
+        },
+
+        child: Text(
+          value,
+          style: TextStyle(
+            color: currentindex == index ? Colors.white : color,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     ));
   }
 }

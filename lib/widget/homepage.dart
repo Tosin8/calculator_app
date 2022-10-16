021d1bf08d2674286d82f799cb7515e164edc4e9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-16 18:17:20 +0100

    Update homepage.dart

--- lib/widget/homepage.dart
+++ lib/widget/homepage.dart
@@ -13,56 +13,106 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 12.0),
-      height: 80.0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'BMI Caculator',
-            style: TextStyle(color: Colors.black),
-          ),
-          elevation: 0.0,
-          backgroundColor: const Color(0xfffafafa),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.settings,
-                  color: Colors.black,
-                )),
-          ],
-        ),
-        body: SingleChildScrollView(
-            child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
+        margin: const EdgeInsets.symmetric(horizontal: 12.0),
+        height: 80.0,
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'BMI Caculator',
+                style: TextStyle(color: Colors.black),
+              ),
+              elevation: 0.0,
+              backgroundColor: const Color(0xfffafafa),
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.settings,
+                      color: Colors.black,
+                    )),
+              ],
+            ),
+            body: SingleChildScrollView(
+                child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        radioButton('Man', Colors.blue, 0),
-                        radioButton('Woman', Colors.pink, 1),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    const Text('Your Height in Cm: ',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                        )),
-                    const TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          hintText: 'Your height in Cm', filled: true),
-                    )
+                        Row(
+                          children: [
+                            radioButton('Man', Colors.blue, 0),
+                            radioButton('Woman', Colors.pink, 1),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        const Text('Your Height in Cm: ',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            )),
+                        TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                              hintText: 'Your height in Cm',
+                              filled: true,
+                              fillColor: Colors.grey[200],
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                borderSide: BorderSide.none,
+                              )),
+                        ),
+                        const SizedBox(height: 20.0),
+                        const Text('Your Weight in Kg: ',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            )),
+                        TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                              hintText: 'Your Weight in Kg',
+                              filled: true,
+                              fillColor: Colors.grey[200],
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                borderSide: BorderSide.none,
+                              )),
+                        ),
+                        const SizedBox(height: 20),
 
-                    // creating the input form.
-                  ],
-                ))),
-      ),
-    );
+                        // creating the input form.
+
+                        // adding the calculate button
+                        SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Container(
+                                color: Colors.blue,
+                                child: const Text('Calculate',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    )),
+                              ),
+                            )),
+
+                        const SizedBox(height: 20.0),
+                        const SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            'Your BMI is: ',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )))));
   }
 
 // declaring a function to change the index value on button pressed.
@@ -78,20 +128,21 @@ class _HomePageState extends State<HomePage> {
   Widget radioButton(String value, Color color, int index) {
     return Expanded(
         child: Container(
-      child: ElevatedButton(
+            child: TextButton(
+      onPressed: () {
+        // clicking the button , the current index will have the value of the button's index.
+        changeIndex(index);
+      },
+      child: Container(
         //  makeing the color of the button change in function of it's selected or not.
 
         color: currentindex == index
             ? color
             : Colors.grey[
                 200], // if the current index is equal to the button index then put the color , the main color that will define otherwise make it grey.
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ), // adding the round button.
-        onPressed: () {
-          // clicking the button , the current index will have the value of the button's index.
-          changeIndex(index);
-        },
+        // shape: RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.circular(8.0),
+        // ), // adding the round button.
 
         child: Text(
           value,
@@ -102,6 +153,6 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-    ));
+    )));
   }
 }
